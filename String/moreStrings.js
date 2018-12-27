9de793b1058d34a77430e5c108f71d761594e424
ZPPD <zhana.p.d@gmail.com>
2018-12-27 13:31:42 -0500

    pig latin Codewars solution

---
+++ String/moreStrings.js
@@ -0,0 +1,14 @@
+/* ===============================
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+===========================*/
+
+function pigIt(str) {
+  //Code here
+  const arr = str.split(" ");
+
+  const output = arr.map(word => word.slice(1) + word.slice(0, 1) + "ay");
+
+  return output.join(" ");
+}
